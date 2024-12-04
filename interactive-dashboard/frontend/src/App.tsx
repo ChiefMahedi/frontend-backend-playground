48c321c2309065fb9213c8c1978510943bce0d93
Shawon <chiefmahedi@gmail.com>
2024-12-04 23:10:01 +0600

    Update App.tsx

--- interactive-dashboard/frontend/src/App.tsx
+++ interactive-dashboard/frontend/src/App.tsx
@@ -1,8 +1,8 @@
-
+import DashboardRouter from "./dashboard/router/DasboardRouter"
 function App() {
   return (
     <>
-    <p className="bg-orange-500 text-white">Interactive Dashboard</p>
+    <DashboardRouter/>
     </>
   )
 }
